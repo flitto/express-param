601cfe5fc3d82978c2cd09e125a5b5f89b8ff310
jays <jays.woo@flitto.com>
2014-09-28 16:32:21 +0900

    add custom req key name option

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 var fetch = require('./lib/fetchParams.js')
 	, checkErr = require('./lib/checkInvalidReq.js');
 
-module.exports = function() {
+module.exports = function(option) {
 	return function(req, res, next) {
-		req.fetchParameter = fetch.fetchParameter.bind({req: req});
+		req.fetchParameter = fetch.fetchParameter.bind({req: req, extraOption: option});
 		req.checkParamErr =  checkErr.checkParamErr;
 
 		return next();
